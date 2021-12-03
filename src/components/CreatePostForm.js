d011e8c769bd39787c7d81e6ce498836abde2658
robwhite <robert.white3120@gmail.com>
2021-12-02 21:48:37 -0600

    added username and userrep to page on login as well as added onChange functionality to create post form

--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -5,17 +5,25 @@ import useLocalStorage from '../useLocalStorage';
 
 const CreatePostForm = (params) => {
 
+     const emptyBody = {title: '', image: '', location: '', author: '', tags: '' }
+
      const [imageURL, setImageURL] = useState("");
      const [imageFile, setImageFile] = useState();
      const [currentUser, setCurrentUser] = useLocalStorage("currentUser");
+     const [body, setBody] = useState(emptyBody)
 
      const handleFileOnChange = (e) => {
           setImageFile(e.target.files['0']);
      }
 
+     // need to add way to add "author" and image field to body
      const handlePostSubmit = (e) => {
           e.preventDefault();
 
+     }
+
+     const handleOnChange = (e) => {
+          setBody({...body, [e.target.name]: e.target.value})
      }
 
      const handleImageUpload = (e) => {
@@ -24,7 +32,7 @@ const CreatePostForm = (params) => {
           formData.append("file", imageFile);
           console.log(formData)
 
-          axios.post("https://localhost:7047/upload", formData)
+          axios.post("https://localhost:7047/upload", formData) // change this address for production
                .then((response) => {
                     console.log(response.data)
                     setImageURL("https://localhost:7047/upload/" + response.data)
@@ -38,10 +46,11 @@ const CreatePostForm = (params) => {
                <h2 className="mt-5 text-2xl font-bold">Share your Locale!</h2>
                <form onSubmit={handlePostSubmit} className="flex flex-col items-center w-full pl-2 pr-2">
                     <label htmlFor="title">Title:</label>
-                    <input type="text" name="title" className="text-center" placeholder="Amazing hiking trail" />
+                    <input type="text" name="title" className="text-center" placeholder="Amazing hiking trail" value={body.title} onChange={handleOnChange}/>
                     <label htmlFor="location">Location:</label>
-                    <input type="text" name="location" placeholder="San Antonio, TX" className="text-center" />
-                    <label htmlFor="tags">Tags:</label>                         <input type="text" name="tags" placeholder="Tags: separate tags with a semicolon (;)" className="w-full text-center" />
+                    <input type="text" name="location" placeholder="San Antonio, TX" className="text-center" value={body.location} onChange={handleOnChange}/>
+                    <label htmlFor="tags">Tags:</label>                         
+                    <input type="text" name="tags" placeholder="Tags: separate tags with a semicolon (;)" className="w-full text-center" value={body.tags} onChange={handleOnChange}/>
                     <input type="hidden" name="author" value={currentUser}/>
                     {imageURL && <input type="submit" value="Create Post" className="mt-3 h-8 w-full rounded text-gray-50 bg-blue-400 hover:bg-blue-500 cursor-pointer"/>}
                </form>
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ const Header = ({submitLogIn}) => {
 
      // REFACTOR:
      const [ currentUser, setCurrentUser ] = useLocalStorage("currentUser");
+     const [ currentUserRep, setCurrentUserRep] = useLocalStorage("currentUserRep")
 
      // if profile icon clicked 
      const handleProfileIconClicked = () => {
@@ -34,7 +35,11 @@ const Header = ({submitLogIn}) => {
                     <Link to="/"><p className="text-gray-50 hover:text-gray-200 cursor-pointer text-6xl font-logo subpixel-antialiased ml-3">Locale</p></Link>
                     
                </div>
-               <h3 onClick={handleProfileIconClicked} className="text-3xl self-center text-gray-50 hover:text-gray-300 mr-3 cursor-pointer"><CgProfile /></h3>
+               <div className="flex flex-col self-center">
+                    <h3 onClick={handleProfileIconClicked} className="text-3xl self-center text-gray-50 hover:text-gray-300 mr-3 cursor-pointer"><CgProfile /></h3>
+                    <h3 className="text-gray-50 mr-3">{currentUser + ": " + currentUserRep}</h3>
+               </div>
+               
                
                {/* this menu appears when a user is not logged in */}
                <div className={ profileIconClicked && !currentUser ? "flex flex-col justify-around items-center login-signup h-44 w-40 bg-gray-50 absolute top-16 right-3 rounded-md shadow-md" : "hidden"}>
